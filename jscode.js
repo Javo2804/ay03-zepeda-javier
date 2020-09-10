5331ee578d5bd9655448a1b88656fcc7a91f1991
Javier Zepeda Avello <javier@MacBook-Pro-de-Javier.local>
2020-09-10 18:29:04 -0300

    edited url

--- jscode.js
+++ jscode.js
@@ -5,7 +5,7 @@ var likes = 0
 var saludos = ["Holaaaaaaa", "estoy chato de la cuarentena :(", "¿Qué pasa pioje?", "Dime porfa que te gustó mi página", "Dame un like con el botón de corazón!", "¡Hola Mundo!", "Lorem Ipsum", "No sé si esto funciona en Internet Explorer", "Con ese botón obtienes un título aleatorio ;)"]
 
 $(document).ready(function(){
-    $.getJSON("https://api.countapi.xyz/get/avello.cl/web0_pv_likes", function(response) {
+    $.getJSON("https://api.countapi.xyz/get/avello.cl/web0_benja_likes", function(response) {
         likes = response.value;
         if(likes==1){
             $("#like_text").text(`${likes} me gusta`);
@@ -42,7 +42,7 @@ $(document).ready(function(){
         console.log("Liked!")
         $("#like_svg_line").css("fill","none")
         $("#like_svg_bg").css("fill","red")
-        $.getJSON("https://api.countapi.xyz/get/avello.cl/web0_pv_likes");
+        $.getJSON("https://api.countapi.xyz/get/avello.cl/web0_benja_likes");
         likes++
         if (likes == 1){
             $("#like_text").text(`${likes} me gusta`)
